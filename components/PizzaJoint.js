0d4235b5bec06662548afb177015a9eb10b4d434
Joseph Elias <jrelias@outlook.com>
2022-01-12 11:48:58 -0500

    updated background color, centered tagline, added pizza joint info to card

--- components/PizzaJoint.js
+++ components/PizzaJoint.js
@@ -1,21 +1,24 @@
 import articleStyles from '../styles/Article.module.css';
 import Image from 'next/image';
-const PizzaJoint = ({ pizzaJoint }) => {
+const PizzaJoint = ({ pizzaJoint, index }) => {
   console.log('THIS IS THE PIZZA JOINT DATA', pizzaJoint);
   return (
     <>
-      <h1>{`Walk ${Math.ceil(
+      {/* <h1>{`Walk ${Math.ceil(
         pizzaJoint.travelDistance * 3.28084
-      )} feet to...`}</h1>
+      )} feet to...`}</h1> */}
       <br></br>
       <div className={articleStyles.card}>
-        <h3>{pizzaJoint.name}</h3>
+        <h4>{`Stop number ${index + 1}`}</h4>
+        <h2>{pizzaJoint.name}</h2>
         <Image
           src={pizzaJoint.image_url}
           alt='Pizza stop image'
           width={500}
           height={500}
         ></Image>
+        <h4>{pizzaJoint.location.display_address[0]}</h4>
+        <h5>{`Yelp rating: ${pizzaJoint.rating}`}</h5>
       </div>
     </>
   );
